1be43e6e0743263a21ea442b2d0ed9cabb6335a7
ossamumaeda <ossamumaeda@gmail.com>
2026-03-02 23:22:11 -0300

    feature - Validations in request dtos

--- src/main/java/com/delivery/api/usecase/dto/ProductCreateRequest.java
+++ src/main/java/com/delivery/api/usecase/dto/ProductCreateRequest.java
@@ -1,12 +1,21 @@
 package com.delivery.api.usecase.dto;
-public record ProductCreateRequest(
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record ProductCreateRequest(
+    
+    @NotNull(message = "Name can't be null")
     String name,
 
+    @NotNull(message = "Price can't be null")
     Long price,
 
+    @NotNull(message = "Stocks can't be null")
+    @Positive
     Integer stockQuantity,
 
+    @NotNull(message = "Code can't be null")
     String code
     
 ) {
